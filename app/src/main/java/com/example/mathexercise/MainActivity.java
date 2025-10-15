26914ea3a988e68c85666d5e166f1176a9bcba80
bhaa <bhaahijazi1805@gmail.com>
2025-10-15 17:43:13 +0300

    s

--- app/src/main/java/com/example/mathexercise/MainActivity.java
+++ app/src/main/java/com/example/mathexercise/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,9 +14,11 @@ public class MainActivity extends AppCompatActivity {
     private int rightCount;
     private TextView wrong;
     private int wrongCount;
-
+    private TextView mark;
+    private int totalMark;
     private TextView num1;
     private TextView num2;
+    private boolean s,t,c;
 
     private Button single;
     private Button till20;
@@ -33,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
 
         rightCount = 0;
         wrongCount = 0;
+        totalMark = 0;
+        s = false;
+        t = false;
+        c = false;
+
 
         right = findViewById(R.id.rightId);
         wrong = findViewById(R.id.wrongId);
@@ -43,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
         challenge = findViewById(R.id.chalngeId);
         check = findViewById(R.id.checkId);
         answer = findViewById(R.id.answerId);
+        mark = findViewById(R.id.markId);
+
 
         single.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 num1.setText(r1+"");
                 num2.setText(r2+"");
                 answer.setText("");
+                s = true;
             }
         });
 
@@ -64,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 num1.setText(r1 + "");
                 num2.setText(r2 + "");
                 answer.setText("");
+                t = true;
             }
         });
 
@@ -75,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 num1.setText(r1+"");
                 num2.setText(r2+"");
                 answer.setText("");
+                c = true;
             }
         });
 
@@ -96,12 +109,42 @@ public class MainActivity extends AppCompatActivity {
                     num1.setText("");
                     num2.setText("");
                     answer.setText("");
+                    if (s) {
+                        totalMark += 2;
+                    } else if (t) {
+                        totalMark += 5;
+                    } else if (c) {
+                        totalMark += 10;
+                    }
+                    if (totalMark > 100) {
+                        totalMark = 100;
+                    }
+                    mark.setText("total mark:"+totalMark);
+                    s = false;
+                    t = false;
+                    c = false;
+                    Toast.makeText(MainActivity.this, "Correct!", Toast.LENGTH_SHORT).show();
                 } else {
                     wrongCount++;
                     wrong.setText("wrong:"+wrongCount);
                     num1.setText("");
                     num2.setText("");
                     answer.setText("");
+                    if (s) {
+                        totalMark -= 2;
+                    } else if (t) {
+                        totalMark -= 5;
+                    } else if (c) {
+                        totalMark -= 10;
+                    }
+                    if (totalMark < 0) {
+                        totalMark = 0;
+                    }
+                    mark.setText("total mark:"+totalMark);
+                    s = false;
+                    t = false;
+                    c = false;
+                    Toast.makeText(MainActivity.this, "Wrong!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
